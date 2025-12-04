38d3f4a1369953b3fed420ebf275dd8919826299
RonySarmiento <ronyj.sarmiento@gmail.com>
2025-12-04 19:42:41 +0000

    Super Trunfo em C.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,31 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+char Estado;
+char Código[20];
+char Nome[30];
+int População, Pontos;
+double Área, PIB;
 
   // Área para entrada de dados
+  printf("Cadastre sua carta\n");
+printf("Digite o Estado: \n");
+scanf("%c", &Estado);
+printf("Digite O Código: \n");
+scanf("%s", Código);
+printf("Digite o nome da cidade: \n");
+scanf("%s", Nome);
+printf("Digite a População: \n");
+scanf("%d", &População);
+printf("Digite o Número de Pontos Turísticos: \n");
+scanf("%d", &Pontos);
+printf("Digite a Área: \n");
+scanf("%lf", &Área);
+printf("Digite o PIB: \n");
+scanf("%lf", &PIB);
+
 
   // Área para exibição dos dados da cidade
-
+printf("Estado: %c\n - Código: %s\n - Nome da Cidade: %s\n - População: %d\n - Pontos Turísticos: %d\n - Área: %.2fkm²\n - PIB: %.2fbilhões de reais\n", Estado, Código, Nome, População, Pontos, Área, PIB);
 return 0;
 } 

5bcd51683534bb6ab4350366aca3b01e70844a42
RonySarmiento <ronyj.sarmiento@gmail.com>
2025-12-07 16:26:34 +0000

    CARTA CHANGES

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,31 +6,35 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-char Estado;
-char Código[20];
-char Nome[30];
-int População, Pontos;
-double Área, PIB;
+  char Estado;
+  char Código[20];
+  char Nome[30];
+  int Populacao, Pontos;
+  double Area, PIB;
+  double Densidade;
+  double xcapita; 
+    // Área para entrada de dados
+      printf("Cadastre sua carta\n");
+      printf("Digite o Estado: \n");
+      scanf("%c", &Estado);
+      printf("Digite O Código: \n");
+      scanf("%s", Código);
+      printf("Digite o nome da cidade: \n");
+      scanf("%s", Nome);
+      printf("Digite a População: \n");
+      scanf("%d", &Populacao);
+      printf("Digite o Número de Pontos Turísticos: \n");
+      scanf("%d", &Pontos);
+      printf("Digite a Área: \n");
+      scanf("%lf", &Area);
+      printf("Digite o PIB: \n");
+      scanf("%lf", &PIB);
 
-  // Área para entrada de dados
-  printf("Cadastre sua carta\n");
-printf("Digite o Estado: \n");
-scanf("%c", &Estado);
-printf("Digite O Código: \n");
-scanf("%s", Código);
-printf("Digite o nome da cidade: \n");
-scanf("%s", Nome);
-printf("Digite a População: \n");
-scanf("%d", &População);
-printf("Digite o Número de Pontos Turísticos: \n");
-scanf("%d", &Pontos);
-printf("Digite a Área: \n");
-scanf("%lf", &Área);
-printf("Digite o PIB: \n");
-scanf("%lf", &PIB);
+      Densidade = (double) (Populacao / Area );
+      xcapita = (double) (PIB / Populacao);
 
 
-  // Área para exibição dos dados da cidade
-printf("Estado: %c\n - Código: %s\n - Nome da Cidade: %s\n - População: %d\n - Pontos Turísticos: %d\n - Área: %.2fkm²\n - PIB: %.2fbilhões de reais\n", Estado, Código, Nome, População, Pontos, Área, PIB);
-return 0;
-} 
+        // Área para exibição dos dados da cidade
+        printf("Estado: %c\n - Código: %s\n - Nome da Cidade: %s\n - População: %d\n - Pontos Turísticos: %d\n - Área: %.2lfkm²\n - PIB: %.2lfbilhões de reais\n - Densidade Populacional é: %.2lf\n - PIB per Capita é: %.2lf\n", Estado, Código, Nome, Populacao, Pontos, Area, PIB, Densidade, xcapita);
+        return 0;
+        } 

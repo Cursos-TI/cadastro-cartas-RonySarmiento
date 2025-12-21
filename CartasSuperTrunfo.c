24e1fa7bd1852273b137e5199b09f266247a5dad
RonySarmiento <ronyj.sarmiento@gmail.com>
2025-12-21 18:37:24 +0000

    CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -35,6 +35,30 @@ int main() {
 
 
         // Área para exibição dos dados da cidade
-        printf("Estado: %c\n - Código: %s\n - Nome da Cidade: %s\n - População: %d\n - Pontos Turísticos: %d\n - Área: %.2lfkm²\n - PIB: %.2lfbilhões de reais\n - Densidade Populacional é: %.2lf\n - PIB per Capita é: %.2lf\n", Estado, Código, Nome, Populacao, Pontos, Area, PIB, Densidade, xcapita);
+        printf("Carta 1\n - Estado: %c\n - Código: %s\n - Nome da Cidade: %s\n - População: %d\n - Pontos Turísticos: %d\n - Área: %.2lfkm²\n - PIB: %.2lfbilhões de reais\n - Densidade Populacional é: %.2lf\n - PIB per Capita é: %.2lf\n", Estado, Código, Nome, Populacao, Pontos, Area, PIB, Densidade, xcapita);
+
+      printf("Cadastre sua carta\n");
+      printf("Digite o Estado: \n");
+      scanf(" %c", &Estado);
+      printf("Digite O Código: \n");
+      scanf("%s", Código);
+      printf("Digite o nome da cidade: \n");
+      scanf("%s", Nome);
+      printf("Digite a População: \n");
+      scanf("%d", &Populacao);
+      printf("Digite o Número de Pontos Turísticos: \n");
+      scanf("%d", &Pontos);
+      printf("Digite a Área: \n");
+      scanf("%lf", &Area);
+      printf("Digite o PIB: \n");
+      scanf("%lf", &PIB);
+
+      Densidade = (double) (Populacao / Area );
+      xcapita = (double) (PIB / Populacao);
+
+      printf("Carta 2\n - Estado: %c\n - Código: %s\n - Nome da Cidade: %s\n - População: %d\n - Pontos Turísticos: %d\n - Área: %.2lfkm²\n - PIB: %.2lfbilhões de reais\n - Densidade Populacional é: %.2lf\n - PIB per Capita é: %.2lf\n", Estado, Código, Nome, Populacao, Pontos, Area, PIB, Densidade, xcapita);
+
         return 0;
+
+
         } 

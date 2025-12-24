7c624de0bd707d13d7101678de7689e623cc4f77
RonySarmiento <ronyj.sarmiento@gmail.com>
2025-12-24 20:42:54 +0000

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,6 +13,9 @@ int main() {
   double Area, PIB;
   double Densidade;
   double xcapita; 
+  double superpoder;
+
+
     // Área para entrada de dados
       printf("Cadastre sua carta\n");
       printf("Digite o Estado: \n");
@@ -32,10 +35,17 @@ int main() {
 
       Densidade = (double) (Populacao / Area );
       xcapita = (double) (PIB / Populacao);
-
-
+superpoder = Populacao + Area + PIB + Densidade + xcapita + Pontos;
+ 
         // Área para exibição dos dados da cidade
-        printf("Carta 1\n - Estado: %c\n - Código: %s\n - Nome da Cidade: %s\n - População: %d\n - Pontos Turísticos: %d\n - Área: %.2lfkm²\n - PIB: %.2lfbilhões de reais\n - Densidade Populacional é: %.2lf\n - PIB per Capita é: %.2lf\n", Estado, Código, Nome, Populacao, Pontos, Area, PIB, Densidade, xcapita);
+        printf("Carta 1\n - Estado: %c\n - Código: %s\n - Nome da Cidade: %s\n - População: %d\n - Pontos Turísticos: %d\n - Área: %.2lfkm²\n - PIB: %.2lfbilhões de reais\n - Densidade Populacional é: %.2lf\n - PIB per Capita é: %.2lf - Super poder: %.2lf\n", Estado, Código, Nome, Populacao, Pontos, Area, PIB, Densidade, xcapita, superpoder);
+int populacaoC1 = Populacao;
+      double areaC1 = Area;
+      double PIBC1 = PIB;
+      int pontosC1 = Pontos;
+      double densidadec1 = Densidade;
+      double xcapitac1 = xcapita;
+      double Superpoderc1 = superpoder;
 
       printf("Cadastre sua carta\n");
       printf("Digite o Estado: \n");
@@ -55,9 +65,24 @@ int main() {
 
       Densidade = (double) (Populacao / Area );
       xcapita = (double) (PIB / Populacao);
+      superpoder = Populacao + Area + PIB + Densidade + xcapita + Pontos;
+      
+      printf("Carta 2\n - Estado: %c\n - Código: %s\n - Nome da Cidade: %s\n - População: %d\n - Pontos Turísticos: %d\n - Área: %.2lfkm²\n - PIB: %.2lfbilhões de reais\n - Densidade Populacional é: %.2lf\n - PIB per Capita é: %.2lf Super poder: %.2f\n", Estado, Código, Nome, Populacao, Pontos, Area, PIB, Densidade, xcapita, superpoder);
+int populacaoC2 = Populacao;
+      double areaC2 = Area;
+      double PIBC2 = PIB;
+      int pontosC2 = Pontos;
+      double densidadec2 = Densidade;
+      double xcapitac2 = xcapita;
+      double Superpoderc2 = superpoder;
 
-      printf("Carta 2\n - Estado: %c\n - Código: %s\n - Nome da Cidade: %s\n - População: %d\n - Pontos Turísticos: %d\n - Área: %.2lfkm²\n - PIB: %.2lfbilhões de reais\n - Densidade Populacional é: %.2lf\n - PIB per Capita é: %.2lf\n", Estado, Código, Nome, Populacao, Pontos, Area, PIB, Densidade, xcapita);
-
+printf(" Carta 1 venceu: %d\n", populacaoC1 > populacaoC2 );
+printf("Carta 1 venceu: %d\n", areaC1 > areaC2);
+printf("Carta 1 venceu: %d\n", PIBC1 > PIBC2);
+printf("Carta 1 venceu: %d\n", pontosC1 > pontosC2);
+printf("Carta 1 venceu: %d\n", densidadec1 > densidadec2);
+printf("Carta 1 venceu: %d\n", xcapitac1 > xcapitac2);
+printf("Carta 1 venceu: %d\n", Superpoderc1 > Superpoderc2);
         return 0;
 
 
